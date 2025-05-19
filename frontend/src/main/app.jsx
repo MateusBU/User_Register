b6ab847d08157ad309388cb323274e0e51e4e01a
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-19 18:48:04 -0300

    Header frontend

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,4 +1,6 @@
 import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
 import React from 'react';
 
 import Logo from '../components/template/logo';
@@ -10,6 +12,7 @@ export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main />
+        <Main icon="home" title="Start"
+            subtitle="Second Project with React"/>
         <Footer />
     </div>
