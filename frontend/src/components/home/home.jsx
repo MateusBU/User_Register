62e71439f3e5c316c1b72be8f50667552672c212
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-19 19:31:40 -0300

    Creation of Home page

---
+++ frontend/src/components/home/home.jsx
@@ -0,0 +1,13 @@
+import Main from '../template/main';
+import React from 'react';
+
+export default props =>
+    <Main icon="home" title="Start"
+        subtitle="Second Project with React">
+        <div className="display-4">Welcome!</div>
+        <hr />
+        <p className="mb-0">
+            A system to demonstrate how to build a registration form, 
+            developed in React!
+        </p>
+    </Main>
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,15 +4,16 @@ import 'font-awesome/css/font-awesome.min.css'
 import React from 'react';
 
 import Logo from '../components/template/logo';
-import Main from '../components/template/main';
+import Home from '../components/home/home';
 import Footer from '../components/template/footer';
 import Nav from '../components/template/nav';
+
+
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav />
-        <Main icon="home" title="Start"
-            subtitle="Second Project with React"/>
+        <Home />
         <Footer />
     </div>
