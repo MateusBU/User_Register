d5647d437a8f93cdf00a963131cc5f1ac0776a5b
Mateus Urtassum <mateusurtassum@gmail.com>
2025-05-20 19:29:52 -0300

    Create renders to modify and include db.json
    For each render creat a new file.

--- frontend/src/components/user/userCrud.jsx
+++ frontend/src/components/user/userCrud.jsx
@@ -44,10 +44,63 @@ export default class UserCrud extends Component{
         list.unshift(user);
         return list;
     }
+
+    updateField(event){
+        const user = {...this.state.user}; //clone
+        user[event.target.name] = event.target.value;
+        this.setState({user});
+    }
+
+    //add in other file
+    renderForm(){
+        return(
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-grup">
+                            <label>Name</label>
+                            <input type="text" className="form-control" 
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Type the name..."/>
+                        </div>
+                    </div>
+                    {/*big device => use all 12 columns
+                       small devices => col-md-6 */}
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail Address</label>
+                            <input type="text" className="form-control" 
+                                name="email"
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Type the email address..."/>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Save
+                        </button>
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render(){
         return(
             <Main {...headerProps}>
-                User Register
+                {this.renderForm()}
             </Main>
         )
     }
